bdccc65cbdc9d88443ecec7effa7e933eeb60de8
sangam00-7 <sangam000007@gmail.com>
2026-05-06 21:25:03 +0545

    Math related function

---
+++ math1.py
@@ -0,0 +1,28 @@
+#Math related function
+#round function
+x=3.99
+y=2
+z=3
+
+result = round(x)
+print(result)
+
+#absolute value function
+a= -5
+result = abs(a)
+print(result)
+
+#power functiom
+ 
+result = pow(y,z)
+print(result)
+
+#maximum value function finds maximum value
+
+result = max(x,y,z)
+print(result)
+
+#minimum value function finds minimum value
+
+result = min(x,y,z)
+print(result)
