e97206e959125f48f701a3b2ab3acdc7965530d8
sangam00-7 <sangam000007@gmail.com>
2026-05-06 21:46:43 +0545

    Math operatos

---
+++ operatos.py
@@ -0,0 +1,41 @@
+#Arithmetic operators
+#addition operator
+
+friends = 0
+#friends = friends + 1
+friends +=1
+print(friends)
+
+#subtraction operator
+
+student = 7
+#student = student - 4
+student -= 4
+print(student)
+
+#multiplication operator
+
+kids = 2
+#kids = kids *2
+kids *= 2
+print(kids)
+
+#divison operator
+
+employee = 12
+#employee = employee /2
+employee /= 2
+print(employee)
+
+#exponent operator
+
+teacher = 3
+#teacher = teacher **3
+teacher **= 3
+print(teacher)
+
+#modulus operator
+daughter = 8
+#daughter = daughter % 3
+daughter %= 3
+print(daughter)
