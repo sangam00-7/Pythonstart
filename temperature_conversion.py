a111c325e7bc5bd9a0b35bb0709585769a6e9ad8
sangam00-7 <sangam000007@gmail.com>
2026-05-07 09:49:19 +0545

    Temperature

--- temperature_conversion.py
+++ temperature_conversion.py
@@ -4,10 +4,10 @@ temperature = float(input("Enter temperature : "))
 unit = input("elsius or  Fahrenheit (c or f ?) ")
 
 if unit == "c" :
-    result = temperature + 33.8
+    result = (9/5)*temperature + 32
     print(f"Temperature is {result} Fahrenheit ")
 elif unit =="f" :
-    result = temperature - 33.8
+    result = (temperature - 32) *(5/9)
     print(f"Temperature is {result} celsius ")
 else :
     print("Invalid unit.")
