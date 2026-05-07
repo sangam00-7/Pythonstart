da0142b0743f80fa1e434c981e6d6fb061adba57
sangam00-7 <sangam000007@gmail.com>
2026-05-07 08:54:20 +0545

    Statements

---
+++ statements.py
@@ -0,0 +1,8 @@
+#if else statement :- 'if' do some code only if some condition is true
+# else do something else
+
+age = int(input("Enter age : "))
+if age >= 18:
+    print("You are eligible voter.")
+else :
+    print("You are not a eligible voter.")
