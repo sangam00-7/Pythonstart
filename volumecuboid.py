8f711abf92a56075c3a33ed445c10c8c58c58b3a
sangam00-7 <sangam000007@gmail.com>
2026-05-11 21:02:56 +0545

    Volume of a cuboid

---
+++ volumecuboid.py
@@ -0,0 +1,9 @@
+# Volume of a cuboid
+
+length = float(input("Enter the length: "))
+breadth = float(input("Enter the breadth: "))
+height = float(input("Enter the height: "))
+
+volume = length * breadth * height
+
+print(f"The volume of the cuboid is {volume}")
