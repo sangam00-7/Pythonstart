56e058157f410b789964c20338345802cfc8568d
sangam00-7 <sangam000007@gmail.com>
2026-05-11 22:17:32 +0545

    Parameter

---
+++ parameter.py
@@ -0,0 +1,5 @@
+length = float(input("Enter lenght :"))
+breadth = float(input("Enter breadth :"))
+result = 2 *(length + breadth)
+unit = input("Enter unit (m/cm) .")
+print(f"Parameter i {result} {unit}")
