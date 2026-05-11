f33daf3bca327443a124761f2e4cc86183bc7c59
sangam00-7 <sangam000007@gmail.com>
2026-05-11 21:11:58 +0545

    Volume of cuboid

--- volumecuboid.py
+++ volumecuboid.py
@@ -3,7 +3,8 @@
 length = float(input("Enter the length: "))
 breadth = float(input("Enter the breadth: "))
 height = float(input("Enter the height: "))
+unit=input("Enter unit (m/cm/k) :")
 
 volume = length * breadth * height
 
-print(f"The volume of the cuboid is {volume}")
+print(f"The volume of the cuboid is {volume} {unit}^3")
