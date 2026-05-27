1a1318bbd33820e3648c1826d508060c6d2d517f
sangam00-7 <sangam000007@gmail.com>
2026-05-27 21:17:08 +0545

    Correction

--- whileloop.py
+++ whileloop.py
@@ -1,5 +1,5 @@
 name = input("Enter your name : ")
 while name == "" :
-    print("you did't entered your name .")
+    print("You did't entered your name. Plase enter correct name : ")
     name = input("Enter your name : ")
 print(f"Hello {name}")
