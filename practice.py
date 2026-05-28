36474f2599356c548b5f28c55f3fefaee5762dfd
sangam00-7 <sangam000007@gmail.com>
2026-05-28 08:50:05 +0545

    practice

--- practice.py
+++ practice.py
@@ -3,3 +3,17 @@
 name = input("Enter name : ")
 name = name.capitalize()
 print(f"Hello {name}.")
+
+
+#title()
+
+name = input("Enter name : ")
+name = name.title()
+print(f"Hello {name}.")
+
+
+#strip and title
+
+name = input("Enter name : ")
+name = name.strip().title()
+print(f"Hello {name}.")
