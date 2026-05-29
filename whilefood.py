b14a502b5a55e1867d493c746b13f6ca46686537
sangam00-7 <sangam000007@gmail.com>
2026-05-29 13:22:21 +0545

    Food

---
+++ whilefood.py
@@ -0,0 +1,5 @@
+food = input("Enter your food (q for quit): ")
+while not food == "q" :
+    print(f"Your favourite food is {food}")
+    food = input("Enter your food (q for quit): ")
+print("Bye")    
