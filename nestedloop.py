80573c4ee8505431c20df068bf8f4ebf49e031cf
sangam00-7 <sangam000007@gmail.com>
2026-05-30 09:03:48 +0545

    Nested loop

---
+++ nestedloop.py
@@ -0,0 +1,8 @@
+#nested loop= A loop within another loop
+#outer loop
+#inner loop
+
+for x in range (3):
+    for y in range (1,10):
+        print(y,end="")     #removes default newline
+    print()      #generated new line
