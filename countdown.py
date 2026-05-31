5624a46f719a131304c61ef6a9c16a022cd31f28
sangam00-7 <sangam000007@gmail.com>
2026-05-31 23:56:18 +0545

    New file

---
+++ countdown.py
@@ -0,0 +1,23 @@
+#Rectangle structure
+rows = int(input("Enter no of rows: "))
+column = int(input("Enter no of columns: "))
+symbol = input("Enter symbol:")
+
+for x in range (rows):
+    for y in range(column):
+        print(symbol,end="a")
+    print()
+
+
+#
+
+
+#Triangle
+
+rows = int(input("Enter no of rows: "))
+symbol = input("Enter symbol:")
+
+for x in range (rows):
+    for y in range(x+1):
+        print(symbol,end="s")
+    print()
