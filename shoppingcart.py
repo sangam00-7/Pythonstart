ee95c589ec6183068892bb309b50837aa043fd29
sangam00-7 <sangam000007@gmail.com>
2026-06-03 17:21:10 +0545

    Shopping cart

---
+++ shoppingcart.py
@@ -0,0 +1,18 @@
+foods=[]
+prices=[]
+total = 0
+while True:
+    food = input("Enter a food to buy(q to quit): ")
+    if food.lower() =="q":
+        break
+    else:
+        price = float(input(f"Enter price of a {food} : "))
+        foods.append(food)
+        prices.append(price)
+print("Your cart")
+for food in foods:
+    print(food,end=" ")
+    print()
+for price in prices:
+    total += price
+print(f"Your total is : {total}")
