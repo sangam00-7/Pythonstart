09d8113bf114c16f0322f27af7604d441a9431d9
sangam00-7 <sangam000007@gmail.com>
2026-06-07 20:09:50 +0545

    Calculation

--- volume.py
+++ volume.py
@@ -4,5 +4,6 @@ side = float(input("Enter the side length of the cube: "))
 unit =input("Enter unit (m/cm/km) : ")
 
 volume = side ** 3
+print("Calculation")
 
 print(f"The volume of the cube is {volume} {unit}^3 .")
