3e0014f8ecc395dcd15d968913aadc7f08bac4fe
sangam00-7 <sangam000007@gmail.com>
2026-06-12 12:57:24 +0545

    Correction

--- circumference.py
+++ circumference.py
@@ -3,17 +3,17 @@ import math
 radius = float(input("Enter radius : "))
 circumference = 2 * math.pi * radius
 
-print(f"Circumferenceof a circle is {circumference}")
+print(f"Circumferenceof a circle is {circumference:.2f}")
 
 #area
 
 radius = float(input("Enter radius of a circle:"))
 area = math.pi * radius **2
-print(f"Area of a circle is{area} m^2")
+print(f"Area of a circle is {area:.2f} m^2")
 
 #hypotenuse of right angle triangle
 
 a=float(input("Enter a: "))
 b=float(input("Enter b:"))
 c= math.sqrt(pow(a,2)+pow(b,2))
-print(c)
+print(f"Hypotenus of right angle triangle is : {c:.2f}")
