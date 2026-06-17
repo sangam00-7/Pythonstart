1a610a73ee9f7d837163cb2bf6affdcec523b130
sangam00-7 <sangam000007@gmail.com>
2026-06-17 11:44:02 +0545

    Movies

---
+++ movies.py
@@ -0,0 +1,8 @@
+movies = []
+mov1= input("Enter name of First movie : ")
+movies.append(mov1)
+mov2= input("Enter name of Second movie : ")
+movies.append(mov2)
+mov3= input("Enter name of Third movie : ")
+movies.append(mov3)
+print(movies)
