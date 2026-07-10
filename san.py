d4d555f1286472ee041e7f74fa6328547fa48169
sangam00-7 <sangam000007@gmail.com>
2026-07-10 20:11:15 +0545

    Name

--- san.py
+++ san.py
@@ -1,4 +1,16 @@
-num = int(input("Enter how many times you want to print your name : "))
-name = input("Enter your name :")
-for num in name :
+num = int(input("Enter how many times you want to print your name: "))
+name = input("Enter your name: ")
+
+for i in range(num):
     print(name)
+
+#using while
+
+num = int(input("Enter how many times you want to print your name: "))
+name = input("Enter your name: ")
+
+count = 1
+
+while count <= num:
+    print(name)
+    count += 1
